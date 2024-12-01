8517c141d5de9a243fe8899079a55e571b5c9412
Youssef <YoussefHabil711@gmail.com>
2024-12-01 02:18:52 +0200

    editing in the previous code to split the servers into TCP and UDP

--- cli.py
+++
@@ -1,7 +0,0 @@
-import time
-from client_handler import active_connections
-
-def display_server_status():
-    while True:
-        print(f"Server running... Active connections: {active_connections}")
-        time.sleep(5)  # Update every 5 seconds
